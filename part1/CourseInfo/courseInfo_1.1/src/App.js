16e7d19f5f4585bb1d2fba370a59a9a5eeb4d39b
merdie <merdiemlb@gmail.com>
2021-10-01 12:46:28 +0100

    feat: add course information exercice

--- part1/CourseInfo/courseInfo_1.1/src/App.js
+++ part1/CourseInfo/courseInfo_1.1/src/App.js
@@ -1,8 +1,32 @@
 import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
 
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  );
+};
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
